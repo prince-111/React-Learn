c9558cf25ff1803d4572a974443c5ea48e8fc086
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-14 19:10:17 +0530

    Add files via upload

---
+++ src/jsx/With.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function With(){
+    return(
+      <h3>use with jsx.</h3>
+    )
+}
+
+export default With;
---
+++ src/jsx/WithOut.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WithOut(){
+    return React.createElement('h3',null,"use without jsx.");
+}
+
+export default WithOut;
